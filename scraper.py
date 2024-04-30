866fef8db1053891e22e5d142e620b5b426ac768
ZakPorat <zakporat2023@gmail.com>
2024-04-29 18:46:48 -0700

    Scraping using GraphQL

--- scraper.py
+++ scraper.py
@@ -1,80 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium_utils.selenium_actions import SeleniumActions
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+import requests
+import json
 
-import time
+url = 'https://offerup.com/api/graphql'
 
-options: webdriver = webdriver.ChromeOptions()
-driver: webdriver = webdriver.Chrome(options=options)
-actions: SeleniumActions = SeleniumActions(driver=driver)
+delivery_flag = 'p'
+distance = 5
+query_value = 'Jordan 4'
+zip_code = '02108'
 
-class OfferUpScraper:
-  def __init__(self, url: str):
-    self.url = url
-    self.shoe_listings = []
+payload = {"operationName":"GetModularFeed","variables":{"debug":False,"searchParams":[{"key":"DELIVERY_FLAGS","value":f"{delivery_flag}"},{"key":"DISTANCE","value":f"{distance}"},{"key":"q","value":f"{query_value}"},{"key":"platform","value":"web"},{"key":"zipcode","value":f"{zip_code}"},{"key":"experiment_id","value":"experimentmodel24"},{"key":"limit","value":"50"},{"key":"searchSessionId","value":"38b6cc92-8ec1-473c-ab26-72cca971b7a7"}]},
+"query":"query GetModularFeed($searchParams: [SearchParam], $debug: Boolean = false) {\n  modularFeed(params: $searchParams, debug: $debug) {\n    analyticsData {\n      requestId\n      searchPerformedEventUniqueId\n      searchSessionId\n      __typename\n    }\n    categoryInfo {\n      categoryId\n      isForcedCategory\n      __typename\n    }\n    feedAdditions\n    filters {\n      ...modularFilterNumericRange\n      ...modularFilterSelectionList\n      __typename\n    }\n    legacyFeedOptions {\n      ...legacyFeedOptionListSelection\n      ...legacyFeedOptionNumericRange\n      __typename\n    }\n    looseTiles {\n      ...modularTileBanner\n      ...modularTileBingAd\n      ...modularTileGoogleDisplayAd\n      ...modularTileJob\n      ...modularTileEmptyState\n      ...modularTileListing\n      ...modularTileLocalDisplayAd\n      ...modularTileSearchAlert\n      ...modularTileSellerAd\n      ...modularModuleTileAdsPostXAd\n      __typename\n    }\n    modules {\n      ...modularGridModule\n      __typename\n    }\n    pageCursor\n    query {\n      ...modularQueryInfo\n      __typename\n    }\n    requestTimeMetadata {\n      resolverComputationTimeSeconds\n      serviceRequestTimeSeconds\n      totalResolverTimeSeconds\n      __typename\n    }\n    searchAlert {\n      alertId\n      alertStatus\n      __typename\n    }\n    debugInformation @include(if: $debug) {\n      rankedListings {\n        listingId\n        attributes {\n          key\n          value\n          __typename\n        }\n        __typename\n      }\n      lastViewedItems {\n        listingId\n        attributes {\n          key\n          value\n          __typename\n        }\n        __typename\n      }\n      categoryAffinities {\n        affinity\n        count\n        decay\n        affinityOwner\n        __typename\n      }\n      rankingStats {\n        key\n        value\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment modularFilterNumericRange on ModularFeedNumericRangeFilter {\n  isExpandedHighlight\n  lowerBound {\n    ...modularFilterNumericRangeBound\n    __typename\n  }\n  shortcutLabel\n  shortcutRank\n  subTitle\n  targetName\n  title\n  type\n  upperBound {\n    ...modularFilterNumericRangeBound\n    __typename\n  }\n  __typename\n}\n\nfragment modularFilterNumericRangeBound on ModularFeedNumericRangeFilterNumericRangeBound {\n  label\n  limit\n  placeholderText\n  targetName\n  value\n  __typename\n}\n\nfragment modularFilterSelectionList on ModularFeedSelectionListFilter {\n  targetName\n  title\n  subTitle\n  shortcutLabel\n  shortcutRank\n  type\n  isExpandedHighlight\n  options {\n    ...modularFilterSelectionListOption\n    __typename\n  }\n  __typename\n}\n\nfragment modularFilterSelectionListOption on ModularFeedSelectionListFilterOption {\n  isDefault\n  isSelected\n  label\n  subLabel\n  value\n  __typename\n}\n\nfragment legacyFeedOptionListSelection on FeedOptionListSelection {\n  label\n  labelShort\n  name\n  options {\n    default\n    label\n    labelShort\n    selected\n    subLabel\n    value\n    __typename\n  }\n  position\n  queryParam\n  type\n  __typename\n}\n\nfragment legacyFeedOptionNumericRange on FeedOptionNumericRange {\n  label\n  labelShort\n  leftQueryParam\n  lowerBound\n  name\n  options {\n    currentValue\n    label\n    textHint\n    __typename\n  }\n  position\n  rightQueryParam\n  type\n  units\n  upperBound\n  __typename\n}\n\nfragment modularTileBanner on ModularFeedTileBanner {\n  tileId\n  tileType\n  title\n  __typename\n}\n\nfragment modularTileBingAd on ModularFeedTileBingAd {\n  tileId\n  bingAd {\n    ouAdId\n    adExperimentId\n    adNetwork\n    adRequestId\n    adTileType\n    adSettings {\n      repeatClickRefractoryPeriodMillis\n      __typename\n    }\n    bingClientId\n    clickFeedbackUrl\n    clickReturnUrl\n    contentUrl\n    deepLinkEnabled\n    experimentDataHash\n    image {\n      height\n      url\n      width\n      __typename\n    }\n    impressionFeedbackUrl\n    impressionUrls\n    viewableImpressionUrls\n    installmentInfo {\n      amount\n      description\n      downPayment\n      __typename\n    }\n    itemName\n    lowPrice\n    price\n    searchId\n    sellerName\n    templateFields {\n      key\n      value\n      __typename\n    }\n    __typename\n  }\n  tileType\n  __typename\n}\n\nfragment modularTileGoogleDisplayAd on ModularFeedTileGoogleDisplayAd {\n  tileId\n  googleDisplayAd {\n    ouAdId\n    additionalSizes\n    adExperimentId\n    adHeight\n    adNetwork\n    adPage\n    adRequestId\n    adTileType\n    adWidth\n    adaptive\n    channel\n    clickFeedbackUrl\n    clientId\n    contentUrl\n    customTargeting {\n      key\n      values\n      __typename\n    }\n    displayAdType\n    errorDrawable {\n      actionPath\n      listImage {\n        height\n        url\n        width\n        __typename\n      }\n      __typename\n    }\n    experimentDataHash\n    formatIds\n    impressionFeedbackUrl\n    personalizationProperties {\n      key\n      values\n      __typename\n    }\n    prebidConfigs {\n      key\n      values {\n        timeout\n        tamSlotUUID\n        liftoffPlacementIDs\n        __typename\n      }\n      __typename\n    }\n    renderLocation\n    searchId\n    searchQuery\n    templateId\n    __typename\n  }\n  tileType\n  __typename\n}\n\nfragment modularTileJob on ModularFeedTileJob {\n  tileId\n  tileType\n  job {\n    address {\n      city\n      state\n      zipcode\n      __typename\n    }\n    companyName\n    datePosted\n    image {\n      height\n      url\n      width\n      __typename\n    }\n    industry\n    jobId\n    jobListingUrl\n    jobOwnerId\n    pills {\n      text\n      type\n      __typename\n    }\n    title\n    apply {\n      method\n      value\n      __typename\n    }\n    wageDisplayValue\n    provider\n    __typename\n  }\n  __typename\n}\n\nfragment modularTileEmptyState on ModularFeedTileEmptyState {\n  tileId\n  tileType\n  title\n  description\n  iconType\n  __typename\n}\n\nfragment modularTileListing on ModularFeedTileListing {\n  tileId\n  listing {\n    ...modularListing\n    __typename\n  }\n  tileType\n  __typename\n}\n\nfragment modularListing on ModularFeedListing {\n  listingId\n  conditionText\n  flags\n  image {\n    height\n    url\n    width\n    __typename\n  }\n  isFirmPrice\n  locationName\n  price\n  title\n  vehicleMiles\n  __typename\n}\n\nfragment modularTileLocalDisplayAd on ModularFeedTileLocalDisplayAd {\n  tileId\n  localDisplayAd {\n    ouAdId\n    adExperimentId\n    adNetwork\n    adRequestId\n    adTileType\n    advertiserId\n    businessName\n    callToAction\n    callToActionType\n    clickFeedbackUrl\n    contentUrl\n    experimentDataHash\n    headline\n    image {\n      height\n      url\n      width\n      __typename\n    }\n    impressionFeedbackUrl\n    searchId\n    __typename\n  }\n  tileType\n  __typename\n}\n\nfragment modularTileSearchAlert on ModularFeedTileSearchAlert {\n  tileId\n  tileType\n  title\n  __typename\n}\n\nfragment modularTileSellerAd on ModularFeedTileSellerAd {\n  tileId\n  listing {\n    ...modularListing\n    __typename\n  }\n  sellerAd {\n    ouAdId\n    adId\n    adExperimentId\n    adNetwork\n    adRequestId\n    adTileType\n    clickFeedbackUrl\n    experimentDataHash\n    impressionFeedbackUrl\n    searchId\n    __typename\n  }\n  tileType\n  __typename\n}\n\nfragment modularModuleTileAdsPostXAd on ModularFeedTileAdsPostXAd {\n  ...modularTileAdsPostXAd\n  moduleId\n  moduleRank\n  moduleType\n  __typename\n}\n\nfragment modularTileAdsPostXAd on ModularFeedTileAdsPostXAd {\n  tileId\n  adsPostXAd {\n    ouAdId\n    adExperimentId\n    adNetwork\n    adRequestId\n    adTileType\n    clickFeedbackUrl\n    experimentDataHash\n    impressionFeedbackUrl\n    searchId\n    offer {\n      beacons {\n        noThanksClick\n        close\n        __typename\n      }\n      title\n      description\n      clickUrl\n      image\n      pixel\n      ctaYes\n      ctaNo\n      __typename\n    }\n    __typename\n  }\n  tileType\n  __typename\n}\n\nfragment modularGridModule on ModularFeedModuleGrid {\n  moduleId\n  collection\n  formFactor\n  grid {\n    actionPath\n    tiles {\n      ...modularModuleTileBingAd\n      ...modularModuleTileGoogleDisplayAd\n      ...modularModuleTileListing\n      ...modularModuleTileLocalDisplayAd\n      ...modularModuleTileSellerAd\n      __typename\n    }\n    __typename\n  }\n  moduleType\n  rank\n  rowIndex\n  searchId\n  subTitle\n  title\n  infoActionPath\n  feedIndex\n  __typename\n}\n\nfragment modularModuleTileBingAd on ModularFeedTileBingAd {\n  ...modularTileBingAd\n  moduleId\n  moduleRank\n  moduleType\n  __typename\n}\n\nfragment modularModuleTileGoogleDisplayAd on ModularFeedTileGoogleDisplayAd {\n  ...modularTileGoogleDisplayAd\n  moduleId\n  moduleRank\n  moduleType\n  __typename\n}\n\nfragment modularModuleTileListing on ModularFeedTileListing {\n  ...modularTileListing\n  moduleId\n  moduleRank\n  moduleType\n  __typename\n}\n\nfragment modularModuleTileLocalDisplayAd on ModularFeedTileLocalDisplayAd {\n  ...modularTileLocalDisplayAd\n  moduleId\n  moduleRank\n  moduleType\n  __typename\n}\n\nfragment modularModuleTileSellerAd on ModularFeedTileSellerAd {\n  ...modularTileSellerAd\n  moduleId\n  moduleRank\n  moduleType\n  __typename\n}\n\nfragment modularQueryInfo on ModularFeedQueryInfo {\n  appliedQuery\n  decisionType\n  originalQuery\n  suggestedQuery\n  __typename\n}\n"}
 
-  def set_location(self, zip_code: int):
-    driver.get(self.url)
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Cookie': 'DX=web-8a3ff805a708131d76d62a800c628c1eae0d3e724ac64018a33fc705; ou.ftue=1; ou.color-mode=light; OU.USER_CONTEXT_COOKIE={%22device_id%22:%22web-8a3ff805a708131d76d62a800c628c1eae0d3e724ac64018a33fc705%22%2C%22user_agent%22:%22Mozilla/5.0%20(Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7)%20AppleWebKit/537.36%20(KHTML%2C%20like%20Gecko)%20Chrome/124.0.0.0%20Safari/537.36%22%2C%22device_platform%22:%22web%22}; ou.location.jobs={%22city%22:%22Clayton%22%2C%22state%22:%22CA%22%2C%22zipCode%22:%2294517%22%2C%22longitude%22:-121.9146%2C%22latitude%22:37.915%2C%22source%22:%22ip%22}; _lr_geo_location_state=CA; _lr_geo_location=US; cf_clearance=BjmUwaCmKOBBr7VcOmwnqe.ufwclAQMjJOF84_eKfVI-1714423642-1.0.1.1-N9584TyXkS052._e7V3nBwpEZNEXPfgAkjZKbtvN9VGy6iSn8wA5NrInq6OSWa.1ESB16OBm2wABh31k_twVng; ou.session-id=web-8a3ff805a708131d76d62a800c628c1eae0d3e724ac64018a33fc705@1714430470176; ou.location={%22city%22:%22Boston%22%2C%22state%22:%22MA%22%2C%22zipCode%22:%2202108%22%2C%22longitude%22:-71.0661193%2C%22latitude%22:42.3548561%2C%22source%22:%22zipcode%22}; __cf_bm=Il77MDpBKP87BESsHTprCZFIbdsTJE5c1ALLNUZvjAo-1714430470-1.0.1.1-NQ3Dm8MlHNNeOCG2IqrYGNmVrGkYQaFpnsBCGacEatevv.cII4wR1zyHvTyUHZj7zzOTQG557s_zWB1u1UkMDA; _dd_s=rum=0&expire=1714431955300; ou.session-time=1714431055494',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'X-User-Context': '{"device_id":"web-8a3ff805a708131d76d62a800c628c1eae0d3e724ac64018a33fc705","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36","device_platform":"web"}',
+    'Origin': 'https://offerup.com',
+    'Referer': 'https://offerup.com/search?q=jordan+4'
+}
 
-    main_page_element = driver.find_element(By.XPATH, '/html/body/div/header/div[2]/div[1]/div[1]/div/div[3]/button/span/span[2]/span')
-    ActionChains(driver).click(main_page_element).perform()
+response = requests.post(url, headers=headers, json=payload)
 
-    zip_code_element = driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/div[3]/div/div/div[3]/div[2]/button')
-    ActionChains(driver).click(zip_code_element).perform()
-
-    input_element = driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/form/div[4]/div/div/div[4]/div[1]/div/div/input')
-    ActionChains(driver).click(input_element).perform()
-
-    input_element.send_keys(Keys.COMMAND, 'a')
-    input_element.send_keys(Keys.DELETE)
-
-    ActionChains(driver).click(input_element).perform()
-
-    input_element.send_keys(zip_code)
-
-    apply_button = driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/form/div[5]/button')
-    ActionChains(driver).click(apply_button).perform()
-
-    time.sleep(1)
-
-    exit_to_main_page = driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/div[1]/button')
-    ActionChains(driver).click(exit_to_main_page).perform()
-
-    time.sleep(5)
-
-  def scrape_listings(self):
-    while True:
-      initial_len = len(self.shoe_listings)
-      i = 1
-      while True:
-        try:
-          xpath = f'/html/body/div/div[5]/div[2]/div[3]/main/div[2]/div/div[1]/div/a[{i}]'
-          post = driver.find_element(By.XPATH, xpath)
-          aria_label = post.get_attribute('aria-label')
-          href = post.get_attribute('href')
-          if href.startswith("https://www.bing.com"): # ad
-            i += 1
-            continue
-          if (aria_label, href) not in self.shoe_listings:
-            self.shoe_listings.append((aria_label, href))
-          i += 1
-        except:
-          break
-      driver.execute_script("window.scrollBy(0, 10000);")
-      driver.implicitly_wait(1)
-      if initial_len == len(self.shoe_listings):
-        break
-
-    driver.quit()
-
-    print(self.shoe_listings)
-
-x = OfferUpScraper('https://offerup.com/search?q=jordan+4&DISTANCE=5&DELIVERY_FLAGS=p')
-x.set_location(94613)
-x.scrape_listings()
-
-
-
-
+if response.status_code == 200:
+    data = response.json()
+    print(json.dumps(data, indent=2))
+else:
+    print("Failed to fetch data:", response.status_code)
