b728409877e454670c0e316ad3851ffe0b7e9c9f
ZakPorat <zakporat2023@gmail.com>
2024-05-04 22:55:22 -0400

    Fetch stockx sneaker data

---
+++ price_data_api/stockx_api.py
@@ -0,0 +1,33 @@
+import requests
+
+def search(query):
+  url = f'https://stockx.com/api/browse?_search={query}'
+
+  headers = {
+    'authority': 'stockx.com',
+    'method': 'GET',
+    'path': f'/api/browse?_search={query}',
+    'scheme': 'https',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'utf-8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'If-None-Match': 'W/"6lx1nbpyam63av"',
+    'Priority': 'u=0, i',
+    'Sec-Ch-Prefers-Color-Scheme': 'dark',
+    'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"macOS"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+  }
+
+  response = requests.get(url=url, headers=headers)
+
+  print(response.text)
+
+search('jordan')
